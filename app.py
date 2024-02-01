aea737772abc70a0471f4def9844851c05eef34b
Heavenboi <nigelzenda98@gmail.com>
2024-02-01 16:24:40 +0200

    added static files

--- app.py
+++ app.py
@@ -1,25 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
-
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '6eef104ab11c0cb779c74b3690ff613a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    username = db.Column(db.string(20), unique=True, nullable=False)
+    email = db.Column(db.string(120), unique=True, nullable=False)
+    image_file = db.Column(db.string(20), unique=True, default='defaut.jpeg')
+    password = db.Column(db.string(60), nullable=False)
+
+jobs = [{
+    'title': 'Frontend developer',
+    'salary': '15200/year',
+    'type': 'Full-Time',
+    'location': 'Los Vegas. USA'
+},
+{
+    'title': 'Backend developer',
+    'salary': '22200/year',
+    'type': 'Full-Time',
+    'location': 'Johannesburg, SA'
+},
+{
+
+    'title': 'Product designer',
+    'salary': '12900/year',
+    'type': 'Full-Time',
+    'location': 'Zimabawe. USA'
+},
+{
+    'title': 'Frontend developer',
+    'salary': '15200/year',
+    'type': 'Full-Time',
+    'location': 'Toronto. Canada'
+}]
 
 @app.route("/")
 def homepage():
-    return render_template('home.html')
+    return render_template('home.html', jobs=jobs)
 
-@app.route("/register")
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/jobs")
+def jobs_list():
+    return render_template('jobs.html', jobs=jobs)
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title = 'Register', form = 'Form')
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('jobs_list'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title = 'Login', form = 'Form')
+    if form.validate_on_submit():
+        if form.email.data == 'wooden@gmail.com' and form.password.data == 'password':
+            flash('You have logged in', 'success')
+            return redirect(url_for('jobs_list'))
+        else:
+            flash('Log in unsuccessful. Please check username and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
--- forms.py
+++ forms.py
@@ -3,18 +3,21 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 #registration form for new users
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', 
+    username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    SubmitField = SubmitField('Sign Up')
-    
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 #log in form for existing users
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    SubmitField = SubmitField('Login')
+    submit = SubmitField('Login')
